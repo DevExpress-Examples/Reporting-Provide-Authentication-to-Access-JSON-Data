d4c074abda47540adc7c27a74a9c9761fac5c129
Andrey Lepikhov (auto) <andrey.lepikhov@devexpress.com>
2019-11-04 23:35:27 +0300

    auto sync for version 2018.2

--- VB/Reporting-Provide-Authentication-to-Access-JSON-Data/Form1.vb
+++ VB/Reporting-Provide-Authentication-to-Access-JSON-Data/Form1.vb
@@ -21,7 +21,7 @@ Namespace XtraReport_JsonDataSource_with_Authorization
         End Sub
         Private Sub RuntimeAuthenticationButton_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button2.Click
             Dim report = (New MyReportHelper()).CreateReport()
-            report.DataSource = JsonDataSourceAuthorization_Example.CreateCustomJsonDataSource("http://northwind.servicestack.net/customers.json", "userName1", "userPassword1")
+            report.DataSource = JsonDataSourceAuthorization_Example.CreateCustomJsonDataSource("https://raw.githubusercontent.com/DevExpress-Examples/DataSources/master/JSON/customers.json", "userName1", "userPassword1")
             report.DataMember = "Customers"
             Call (New DevExpress.XtraReports.UI.ReportDesignTool(report)).ShowDesigner()
         End Sub
@@ -33,13 +33,13 @@ Namespace XtraReport_JsonDataSource_with_Authorization
 
 
         Public Shared Function CreateCustomJsonDataSource(ByVal uri As String, ByVal userName As String, ByVal password As String) As JsonDataSource
-            Dim jsonDatasource = New JsonDataSource() With {
-                .Name = "jsonDataSource",
-                .JsonSource = New MyUriJsonSource() With {
-                    .Uri = New Uri(uri),
-                    .Username = userName,
-                    .Password = password
-                }
+            Dim jsonDatasource = New JsonDataSource() With { _
+                .Name = "jsonDataSource", _
+                .JsonSource = New MyUriJsonSource() With { _
+                    .Uri = New Uri(uri), _
+                    .Username = userName, _
+                    .Password = password _
+                } _
             }
             Return jsonDatasource
         End Function
